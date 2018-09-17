0d9449e748050f94df26de48d6ee66325636d325
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-09-17 00:24:05 -0700

    Attempt to make Webpack work, but it doesn't rn

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/agency.js',
+  entry: [
+    './js/agency.js'
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
